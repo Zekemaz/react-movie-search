9f9a7cde0eafbe589f191d8f8dbbb4460a0a6156
Gabe <desseresusinigabriel@gmail.com>
2020-07-01 15:44:16 +0200

    Updates

---
+++ precache-manifest.df93b66b57c8df9a9e571ae25859f340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ff7c305f8e53939ae188ab45c95cd8",
+    "url": "/react-movie-search/index.html"
+  },
+  {
+    "revision": "323b1fa0cdcaf749182a",
+    "url": "/react-movie-search/static/css/main.2dc75072.chunk.css"
+  },
+  {
+    "revision": "e6f7e8dc154c2211819b",
+    "url": "/react-movie-search/static/js/2.0646e4b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-search/static/js/2.0646e4b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323b1fa0cdcaf749182a",
+    "url": "/react-movie-search/static/js/main.291be78f.chunk.js"
+  },
+  {
+    "revision": "ec1afc3891ded9c1372a",
+    "url": "/react-movie-search/static/js/runtime-main.33682eb9.js"
+  }
+]);
